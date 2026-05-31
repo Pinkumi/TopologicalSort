7ef8e28a7d693ca629ef85622f8002e6236c5ebe
Artemio-lab <villalobos1199190@uabc.edu.mx>
2026-05-31 15:48:10 -0700

    Se termino el metodo topologicalSort, con comentarios sobre su funcionamiento y se corrigieron errores de la clase nodo

--- src/main/java/com/topologicalsort/GUI/Controller.java
+++ src/main/java/com/topologicalsort/GUI/Controller.java
@@ -1,10 +1,42 @@
 package com.topologicalsort.GUI;
 
+import com.topologicalsort.Logic.GrafoDirigidoAciclico;
+import com.topologicalsort.Tools.Nodo;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
 /**
  * Conexion entre vista y logica del grafo
  *
  */
 
 public class Controller {
+
+    private GrafoDirigidoAciclico logica;
+    private View vista;
+
+    public Controller(){
+        vista = new View();
+    }
+
+    public void crearGrafoNuevo(int n, String tipo){
+        if (tipo.equals("Integer")) {
+            logica = new GrafoDirigidoAciclico<Integer>(n);
+        } else {
+            logica = new GrafoDirigidoAciclico<String>(n);
+        }
+    }
+
+    public void inicializarBotones(){
+
+    }
+
+    public void actualizarNodos(){
+        vista.actualizarNodos(logica.getNodos());
+    }
+
+
+
 
 }
---
+++ src/main/java/com/topologicalsort/GUI/View.java
@@ -0,0 +1,16 @@
+package com.topologicalsort.GUI;
+
+import com.topologicalsort.Tools.Nodo;
+
+import java.util.ArrayList;
+
+public class View {
+
+    public View(){
+
+    }
+
+    public void actualizarNodos(ArrayList<Nodo> nodos){
+
+    }
+}
